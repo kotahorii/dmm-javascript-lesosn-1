3481949eb33f95a73fd6737004a6dec15080e567
kotahorii <khorii.micron@gmail.com>
2021-11-05 22:07:08 +0900

    finish lesson5

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,6 +1,26 @@
-// $(document).ready(()=>{
-//   $('body').html('<h1>Hello jQuery!!</h1>');
+// $(()=>{
+//   $('.box1').css({
+//     'background-color':'#0000FF',
+//     'height':'100px'
+//   })
 // })
+// $(()=>{
+//   $('.box1').slideDown();
+// })
+// $(()=>{
+//   $('.box1').slideUp()
+// })
+
+// $(()=>{
+//   $('.box1').show();
+//   $('.box1').css({'background-color':'#0000FF'})
+// })
+
+// $(()=>{
+//   $('.box1').hide();
+// })
+
 $(()=>{
-  $('body').html('<h1>Hello jQuery</h1>')
+  $('.box1').slideDown(10000,()=>{$('.box1').css({'background-color':'#0000FF',  'width': '200px','height': '100px'})})
+  .slideUp()
 })
