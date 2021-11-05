340b7c3aa27972d489b841e8c795a31501f75ef7
kotahorii <khorii.micron@gmail.com>
2021-11-05 20:07:34 +0900

    initial commit

---
+++ lesson2/app.js
@@ -0,0 +1,39 @@
+// let hello = "Hello world";
+// alert(hello)
+
+// let int1 = 1;
+// let int2 = -10;
+// let float1 = 3.14;
+
+// let str1 = 'JavaScript'
+// alert(`${str1}` + `${str2}`)
+
+// let orange = 100;
+// let apple = 120;
+
+// if (orange < apple) {
+//   alert('orange is cheaper than apple')
+// } else if (orange === apple) {
+//   alert('orange equals to apple')
+// } else {
+//   alert('orange is more expensive than apple')
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while (num < max) {
+//   num = num * 2
+//   count += 1
+// }
+
+// alert(`2をかけつづけて${max}を超えるのに必要だった回数は${count}回です`)
+
+let i;
+let num = 0
+for (i = 1; i < 11; i++) {
+  num += i
+}
+
+alert(`1から10まで足し算した結果は${num}です`)
