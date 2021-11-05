8b72893048ec1b94b7ae80f9973a8ed82206a449
kotahorii <khorii.micron@gmail.com>
2021-11-05 20:19:45 +0900

    finish variant lesson

---
+++ practice-variable/sample.js
@@ -0,0 +1,55 @@
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// var str = "webcamp" //グローバルスコープ
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello" //関数スコープ
+// }
+
+// foo()
+// console.log(y)
+
+// const foo = () => {
+//   let x = 'webcamp'
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+// for (let i=0; i<10; i++) {
+//   console.log(i)
+// }
+// console.log(i)
+
+var str = "webcamp"
+
+function foo() { 
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
