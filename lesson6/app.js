9e99adc1beb45ed3e28314751229dd84cdc8ee6c
kotahorii <khorii.micron@gmail.com>
2021-11-06 14:13:10 +0900

    lesson6 start

---
+++ lesson6/app.js
@@ -0,0 +1,9 @@
+// $(()=>{
+//   $('.box1').mouseover(()=>{
+//     $('.box1').css({'background-color':'#0000ff'});
+//   });
+//   $('.box1').mouseout(()=>{
+//     $('.box1').css({'background-color':'#ff0000'})
+//   })
+// })
+
